b2d52ee8f4b786735ee262ed964331aa12d423cd
soczystys <34406205+soczystys@users.noreply.github.com>
2023-02-24 12:05:16 +0100

    Stworzenie 2 obiektów Monitor

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,7 +3,21 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Monitor monitor1 = new Monitor();
 
+        monitor1.resolution = "1920x1080";
+        monitor1.broken = false;
+        monitor1.aspectRatio = "16:9";
+        monitor1.screenSize = 23;
+        monitor1.modelName = "M2350D-PZ";
+
+        Monitor monitor2 = new Monitor();
+
+        monitor2.resolution = "3840x2160";
+        monitor2.broken = false;
+        monitor2.aspectRatio = "16:9";
+        monitor2.screenSize = 31.5;
+        monitor2.modelName = " 32UP550N-W";
 
     }
 }
